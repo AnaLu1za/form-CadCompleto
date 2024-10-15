0b73fd2ca4a3a61be2116183caa202d62a03729b
AnaLuiza <aninha141108@gmail.com>
2024-10-15 19:48:54 -0300

    Adiciona parte do código responsável por validar o primeiro dígito verificador do CPF. Ela implementa o algoritmo padrão para calcular esse dígito e compará-lo com o valor presente no CPF informado.

--- script.js
+++ script.js
@@ -56,3 +56,20 @@ loginForm.addEventListener('submit', (event) => {
   
         let soma = 0;
         let resto;
+        
+    // Valida o primeiro dígito verificador
+    for (let i = 1; i <= 9; i++) {
+        soma += parseInt(cpf.substring(i - 1, i)) * (11 - i);
+    }
+
+    resto = (soma * 10) % 11;
+
+    if (resto === 10 || resto === 11) {
+        resto = 0;
+    }
+
+    if (resto !== parseInt(cpf.substring(9, 10))) {
+        return false;
+    }
+
+    soma = 0;
