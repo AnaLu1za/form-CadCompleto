30a65ffb4e34b32c1e27cbf86109cb837558afa9
AnaLuiza <aninha141108@gmail.com>
2024-10-15 19:53:13 -0300

    Adiciona código responsável por validar o segundo dígito verificador do CPF. Ele segue uma lógica similar à validação do primeiro dígito, mas com pesos diferentes.

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ loginForm.addEventListener('submit', (event) => {
   
         let soma = 0;
         let resto;
-        
+
     // Valida o primeiro dígito verificador
     for (let i = 1; i <= 9; i++) {
         soma += parseInt(cpf.substring(i - 1, i)) * (11 - i);
@@ -73,3 +73,22 @@ loginForm.addEventListener('submit', (event) => {
     }
 
     soma = 0;
+    
+    // Valida o segundo dígito verificador
+    for (let i = 1; i <= 10; i++) {
+        soma += parseInt(cpf.substring(i - 1, i)) * (12 - i);
+     }
+      
+    resto = (soma * 10) % 11;
+      
+    if (resto === 10 || resto === 11) {
+        resto = 0;
+     }
+      
+    if (resto !== parseInt(cpf.substring(10, 11))) {
+        return false;
+    }
+      
+        return true;
+    }
+      
