bccd78c51f0ec130535b2dc2b467acc826625f38
AnaLuiza <aninha141108@gmail.com>
2024-10-15 19:41:34 -0300

    Adiciona codigo em JavaScript responsavel por selecionar e armazenar referências a vários elementos HTML dentro do formulário de login.

---
+++ script.js
@@ -0,0 +1,15 @@
+// Seleciona os elementos do formulário
+const emailInput = document.getElementById('Email'); 
+const passwordInput = document.getElementById('password');
+const nameInput = document.getElementById('name');
+const CPFInput = document.getElementById('CPF');
+const data_nasInput = document.getElementById('data_nas');
+const CEPInput = document.getElementById('CEP');
+const ruaInput = document.getElementById('rua');
+const numeroInput = document.getElementById('numero');
+const bairroInput = document.getElementById('bairro');
+const complementoInput = document.getElementById('complemento');
+const cidadeInput = document.getElementById('cidade');
+const estadoInput = document.getElementById('estado');
+
+const loginForm = document.getElementById('login_form');
